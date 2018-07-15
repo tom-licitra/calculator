0dfbee4863b933910c3d60f04a31b729bb2f2a28
Tom Licitra <tom.licitra@gmail.com>
2018-07-15 15:27:31 -0400

    cleaned up specs

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -9,30 +9,22 @@ describe('calculate', () => {
   });
 
   it('can add two numbers together', () => {
-    // const calculator = new Calculate();
     expect(calculator.add(3,4)).to.equal(7);
   });
 
   it('can multiply two numbers together', () => {
-    // const calculator = new Calculate();
     expect(calculator.multiply(3,4)).to.equal(12);
   });
 
   it('can subtract two numbers together', () => {
-    // const calculator = new Calculate();
-    expect(calculator.subtract(4,4)).to.equal(0);
+    expect(calculator.subtract(4, 4)).to.equal(0);
   });
 
   it('can add many numbers together', () => {
-    // const calculator = new Calculate();
     expect(calculator.addFlex(4,4,1,1,1,1)).to.equal(12);
   });
 
   it('can multiply many numbers together', () => {
-    // const calculator = new Calculate();
     expect(calculator.multFlex(4,4,2,1)).to.equal(32);
   });
-
-
-
-});
+})
